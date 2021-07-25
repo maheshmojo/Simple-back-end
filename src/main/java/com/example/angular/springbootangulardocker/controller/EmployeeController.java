ac15c6c48660487b64584fc8c72da8051c8fe344
Mahesh waran <33809529+maheshmojo@users.noreply.github.com>
2021-07-26 01:22:56 +0530

    Update EmployeeController.java

--- src/main/java/com/example/angular/springbootangulardocker/controller/EmployeeController.java
+++ src/main/java/com/example/angular/springbootangulardocker/controller/EmployeeController.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class EmployeeController {
