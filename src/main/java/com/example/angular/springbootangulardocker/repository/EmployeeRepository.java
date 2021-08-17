8ebb38150cf68f27aa1c912a9f405d286aa3df88
Mahesh waran <33809529+maheshmojo@users.noreply.github.com>
2021-08-17 15:47:05 +0530

    Update EmployeeRepository.java

--- src/main/java/com/example/angular/springbootangulardocker/repository/EmployeeRepository.java
+++ src/main/java/com/example/angular/springbootangulardocker/repository/EmployeeRepository.java
@@ -3,7 +3,7 @@ package com.example.angular.springbootangulardocker.repository;
 import com.example.angular.springbootangulardocker.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+/*start*/
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 }
