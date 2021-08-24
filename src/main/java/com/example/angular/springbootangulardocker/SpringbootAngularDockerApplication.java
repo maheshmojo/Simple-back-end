2e834fdb7e533da2b44ffcd0ac727f8808b0adff
Mahesh waran <33809529+maheshmojo@users.noreply.github.com>
2021-08-25 01:21:27 +0530

    Update SpringbootAngularDockerApplication.java

--- src/main/java/com/example/angular/springbootangulardocker/SpringbootAngularDockerApplication.java
+++ src/main/java/com/example/angular/springbootangulardocker/SpringbootAngularDockerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootAngularDockerApplication {
 
 	public static void main(String[] args) {
+		/*hello*/
 		SpringApplication.run(SpringbootAngularDockerApplication.class, args);
 	}
 
